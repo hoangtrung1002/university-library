63245249f730a06d803a434b5850c040739f4907
hoangtrung1002 <n.jordan1002@gmail.com>
2025-01-13 22:05:35 +0700

    ui:auth page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validation";
+
+const SignIn = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{ email: "", password: "" }}
+    onSubmit={() => {}}
+  />
+);
+export default SignIn;
