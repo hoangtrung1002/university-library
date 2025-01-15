d7234f6827b915e51cd991ae786f28c0495f9d35
hoangtrung1002 <n.jordan1002@gmail.com>
2025-01-15 17:38:15 +0700

    ci:setup upstash redis

---
+++ lib/ratelimit.ts
@@ -0,0 +1,11 @@
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import redis from "@/database/redis";
+
+// Create a new ratelimiter, that allows 5 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.fixedWindow(5, "1m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+export default ratelimit;
